1a9da6a9c9d7edee890651877b5846ccb7ec7e00
Kamil <rehaurd@mail.ru>
2020-06-30 23:01:54 +0300

    Initial Commit

---
+++ SwiftUITests/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITests
+//
+//  Created by Камиль on 30.06.2020.
+//  Copyright © 2020 Kamil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
